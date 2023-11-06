4a1d72081e1d773d1ef8140ff7ae33bfd59b9c6c
David Ade <davemoneymachine@gmail.com>
2023-11-06 05:50:58 +0100

    [Update]: initial loader

--- src/App.jsx
+++ src/App.jsx
@@ -12,28 +12,51 @@ import ScrollToTop from './components/ScrollToTop';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { ContextProvider } from './Contexts/Context';
+import { useEffect, useState } from 'react';
+import { PulseLoader } from 'react-spinners';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  }, []);
+
   return (
     <>
       <ContextProvider>
-        <div>
-          <ToastContainer />
-          <ScrollToTop />
-          <Header />
+        {loading ? (
+          <div className="flex w-full h-screen items-center justify-center overflow-hidden">
+            <PulseLoader
+              color={'#1e72bc'}
+              loading={loading}
+              size={30}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+          </div>
+        ) : (
+          <div>
+            <ToastContainer />
+            <ScrollToTop />
+            <Header />
 
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/service" element={<Service />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/pricing" element={<Pricing />} />
-            <Route path="/team" element={<Team />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/service" element={<Service />} />
+              <Route path="/blog" element={<Blog />} />
+              <Route path="/pricing" element={<Pricing />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
 
-          <Footer />
-        </div>
+            <Footer />
+          </div>
+        )}
       </ContextProvider>
     </>
   );
